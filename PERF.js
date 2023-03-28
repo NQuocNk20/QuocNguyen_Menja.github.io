5d11144fa64f705fd075c6dbd7d3201ad4fb485d
NQuocNk20 <102974265+NQuocNk20@users.noreply.github.com>
2023-03-28 23:20:38 +0700

    Add files via upload

---
+++ PERF.js
@@ -0,0 +1,19 @@
+
+// PERF.js
+
+// ============================================================================
+
+// ============================================================================
+
+
+// Dummy no-op functions.
+
+// I use these in a special build for custom performance profiling.
+
+const PERF_START = () => {};
+
+const PERF_END = () => {};
+
+const PERF_UPDATE = () => {};
+
+
